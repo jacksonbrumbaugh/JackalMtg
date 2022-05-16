e8f62bd5cabedb02118b339e3e3de060108e7182
Jackson Brumbaugh <jcb5582@gmail.com>
2022-05-16 06:21:55 -0400

    Changed piped foreach-object to a foreach loop

--- Update-TcgPricing.ps1
+++ Update-TcgPricing.ps1
@@ -1,11 +1,9 @@
 function Update-TcgPricing {
   [CmdletBinding()]
   param(
-    [double]$Discount = 10,
-
     [double]
-    [Alias("Ship", "Pkg")]
-    $ShippingCost = 0.86
+    [Alias("Off")]
+    $Discount = 10
   ) #END block::param
 
   process {
@@ -18,16 +16,35 @@ function Update-TcgPricing {
     $Params = (Get-Content $ParamFilePath) | ConvertFrom-JSON
 
     $DownloadsPath = $Params.DownloadsDir
-    if ( -not(Test-Path $DownloadsPath) ) {
-      throw ( "Failed to find the Downloads folder : " + $DownloadsPath )
+    $UpdatesDir = $Params.UpdatesDir
+    $UpdatedDir = $Params.UpdatedDir
+
+    @(
+      $DownloadsPath,
+      $UpdatesDir,
+      $UpdatedDir
+    ) | ForEach-Object {
+      if ( -not(Test-Path $_) ) {
+        throw ( "Failed to find a needed dir : " + $_ )
+      }
     }
 
-    $UpdatedDir = $Params.UpdatesDir
-    if ( -not(Test-Path $UpdatedDir) ) {
-      throw ( "Failed to locate the Updates folder : " + $UpdatedDir )
+    $ShippingCost = $Params.ShippingCost -as [double]
+    $MinimumPrice = $Params.MinimumPrice -as [double]
+
+    ($ShippingCost, $MinimumPrice) | ForEach-Object {
+      if ( -not($_) ) {
+        throw ( "A needed parameter price was not set in the JSON Params file" )
+      }
     }
 
     $NowSet = $Params.CurrentSet
+
+    if ( -not($NowSet) ) {
+      throw ( "The current set was not listed in the JSON Params file" )
+    }
+
+    Write-Host ( "Using current set as: " + $NowSet )
 
     $TcgExportFile = ( Get-ChildItem $DownloadsPath\TCG*Pricing*csv |
       Sort-Object -Descending LastWriteTime | Select-Object -First 1 -ExpandProperty FullName
@@ -37,22 +54,23 @@ function Update-TcgPricing {
       throw ( "Failed to locate a TCG Player Pricing Export CSV file in " + $DownloadsPath )
     }
 
+    Write-Host "Loading inventory from the CSV file"
     $InventoryList = Import-CSV $TcgExportFile
 
     $Multiplier = 1
+
+    if ( $Discount -gt 100 ) {
+      throw "Cannot offer a discount above 100%"
+    }
 
     if ( $Discount -gt 1 ) {
       $Discount = $Discount / 100
     }
 
-    if ( $Discount -gt 1 ) {
-      throw "Cannot offer a discount above 100%"
-    }
-
     if ( $Discount ) {
       $Multiplier = 1 - $Discount
     } else {
-      $Discount = "0.0"
+      $Discount = 0
     }
 
     Write-Verbose ( "A discount of " + $Discount + " off TCG Market Price will be applied" )
@@ -63,13 +81,21 @@ function Update-TcgPricing {
       TcgLowPrice = "TCG Low Price With Shipping"
       MyPrice     = "TCG MarketPlace Price"
       SetName     = "Set Name"
+      Qty         = "Total Quantity"
     }
 
-    $InventoryList | ForEach-Object {
-      $CardName = $_.($TcgKeysHash.TcgProdName)
-      $TcgMktPrice = $_.($TcgKeysHash.TcgMktPrice) -as [double]
-      $TcgLowPrice = $_.($TcgKeysHash.TcgLowPrice) -as [double]
-      $CardSet = $_.($TcgKeysHash.SetName)
+    Write-Host "Determining pricing for each card in inventory"
+
+    foreach ( $Card in $InventoryList ) {
+      $CardName = $Card.($TcgKeysHash.TcgProdName)
+      $TcgMktPrice = $Card.($TcgKeysHash.TcgMktPrice) -as [double]
+      $TcgLowPrice = $Card.($TcgKeysHash.TcgLowPrice) -as [double]
+      $CardSet = $Card.($TcgKeysHash.SetName)
+      $CardQty = $Card.($TcgKeysHash.Qty) -as [int]
+
+      if ( $CardQty -eq 0 ) {
+        continue
+      }
 
       if ( -not($TcgMktPrice) ) {
         Write-Warning ( "Failed to grab TCG Market Price for " + $CardName )
@@ -77,6 +103,28 @@ function Update-TcgPricing {
       }
 
       $DiscountPrice = [Math]::Floor( 100 * $Multiplier * $TcgMktPrice ) / 100
+
+      $MinChecks = @(
+        @{ Type = "Discounted"; Price = $DiscountPrice },
+        @{ Type = "TCG MKT";    Price = $TcgMktPrice }
+      )
+
+      foreach ( $MinCheck in $MinChecks ) {
+        $CheckPrice = $MinCheck.Price
+        $CheckType = $MinCheck.Type
+        if ( $CheckPrice -lt $MinimumPrice ) {
+          Write-Warning (
+            $CardName + "would have a " + $CheckType  + " price of " + $CheckPrice +
+            " but the min of " + $MinimumPrice + "was set instead"
+          )
+          if ( $CheckType -eq "Discounted" ) {
+            $DiscountPrice = $MinimumPrice
+          }
+          if ( $CheckType -eq "TCG MKT" ) {
+            $TcgMktPrice = $MinimumPrice
+          }
+        }
+      }
 
       $TargetPrice = if ( $CardSet -eq $NowSet ) {
         $TcgMktPrice
@@ -86,24 +134,41 @@ function Update-TcgPricing {
 
       $NewPrice = [Math]::Max( $TargetPrice, ($TcgLowPrice - $ShippingCost) )
 
-      $_.($TcgKeysHash.MyPrice) = $NewPrice
+      $Card.($TcgKeysHash.MyPrice) = $NewPrice
     }
 
-    $UpdatedFileName = "UpdatedPricing_{0}.csv" -f (Get-Date).ToString("yyyy-MM-dd")
-    $UpdatedFilePath = Join-Path $UpdatedDir $UpdatedFileName
+    $DateStamp = (Get-Date).ToString("yyyy-MM-dd")
+    $UpdatesFileName = "UpdatedPricing_{0}.csv" -f $DateStamp
 
-    $InventoryList | Export-CSV -NTI -Path $UpdatedFilePath
+    $ShortAlphabet = ('B', 'C', 'D', 'E', 'F')
+    $n = 0
+    do {
+      $CheckFilePath = Join-Path $UpdatedDir $UpdatesFileName
+      if ( Test-Path $CheckFilePath ) {
+        if ( $n -eq 0 ) {
+          Write-Host "Determining output file name"
+        }
+        $Letter = $ShortAlphabet[$n++]
+        $NewStamp = $DateStamp + '.' + $Letter
+        $UpdatesFileName = $UpdatesFileName -replace $DateStamp, $NewStamp
+      }
+    } while ( Test-Path $CheckFilePath )
+
+    $UpdatesFilePath = Join-Path $UpdatesDir $UpdatesFileName
+
+    Write-Host "Exporting updated pricing CSV file"
+    $InventoryList | Export-CSV -NTI -Path $UpdatesFilePath
 
     $Result = [PSCustomObject]@{
-      FileName = $UpdatedFileName
-      Creation = Test-Path $UpdatedFilePath
+      FileName = $UpdatesFileName
+      Creation = Test-Path $UpdatesFilePath
     }
 
     if ( -not($Result.Creation) ) {
       Write-Warning ( "Failed to create an updated pricing CSV file" )
     } else {
       Remove-Item $TcgExportFile
-      Invoke-Item $UpdatedDir
+      Invoke-Item $UpdatesDir
     }
 
     Write-Output $Result
