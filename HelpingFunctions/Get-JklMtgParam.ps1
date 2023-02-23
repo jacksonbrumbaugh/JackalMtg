b3756f4ddb762a1fc459020c42fb961716075b27
Jackson Brumbaugh <jcb5582@gmail.com>
2023-02-23 04:02:36 -0500

    Add Username parameter

--- HelpingFunctions/Get-JklMtgParam.ps1
+++ HelpingFunctions/Get-JklMtgParam.ps1
@@ -1,11 +1,16 @@
-function Get-JklMtgParam {
-  $ParamFilePath = Join-Path $PSScriptRoot "JackalMtgParams.json"
+function Get-JklMtgParam ( [string]$Username = $env:USERNAME ) {
+  $ParamFilePath = Join-Path $ModuleRootDir "JackalMtgParams.json"
 
   if ( -not(Test-Path $ParamFilePath) ) {
     throw ( "Failed to find the parameter file : " + $ParamFilePath )
   }
 
-  $OutputObject = ( Get-Content $ParamFilePath ) | ConvertFrom-Json
+  $OutputObject = ( ( Get-Content $ParamFilePath ) | ConvertFrom-Json).$Username
+
+  if ( [string]::IsNullOrEmpty($OutputObject) ) {
+    throw ( "Failed to find MTG parameters for the username " + $Username + " configured in the JackalMtgParams.json file. " )
+  }
 
   Write-Output $OutputObject
-}
+
+} # End function
