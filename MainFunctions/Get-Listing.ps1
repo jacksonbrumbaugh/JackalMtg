5a24a38fd826c116a9be0f51e6ad3b740b8c00a9
Jackson Brumbaugh <jcb5582@gmail.com>
2023-04-11 07:30:25 -0400

    Init function

---
+++ MainFunctions/Get-Listing.ps1
@@ -0,0 +1,13 @@
+function Get-Listing {
+  [CmdletBinding()]
+  param ()
+
+  process {
+    Write-Warning "This function is still being developed. "
+
+    <#
+    Want to use date as input to load the *.csv listing file from J:\MTG\TCGSeller\FaceBook-MtgMartketPlace\
+    #>
+  }
+
+} # End function
---
+++ MainFunctions/Get-OrderProduct.ps1
@@ -0,0 +1,17 @@
+function Get-OrderProduct {
+  [CmdletBinding()]
+  param ()
+
+  process {
+    Write-Warning "This function is still being developed. "
+
+    <#
+    After running Get-Listing
+    want to use an input array of fuzzy card names to output the info for TCG Sales > CardsSold
+    e.g.
+    Magic - The Brothers' War: Urza, Lord Protector - #225 - Near Mint [Foil]
+    the [Foil] bit only shows when appropriate
+    #>
+  }
+
+} # End function
